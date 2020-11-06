f14adb9a2c4382f00b996955e2b1f5b47ca96ffb
Kishor Naik <kishor.naik011.net@outlook.com>
2020-11-06 14:46:10 +0530

    Code Commit

---
+++ Sol_Demo/WebApi2/Controllers/DemoController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi2.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/demo")]
+    [ApiController]
+    public class DemoController : ControllerBase
+    {
+        [HttpGet("get")]
+        public IActionResult GetData()
+        {
+            return base.Ok("Hello from Web Api 2");
+        }
+    }
+}
